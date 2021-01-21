1d6a1e09381fbe622c1eb4d909dcacc295262074
nthieu29 <ngotrunghieu@live.com>
2021-01-21 21:49:21 +0700

    init project

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# terraform-example
+# Basic Terraform Example
+
+Configuration in this directory creates:
+
+- An EC2 instance works as a simple HTTP Web Server on port 80.
+- A Security Group which allow inbound connections on port 80 (HTTP) and 22 (SSH). Allow all outbound connections.
+
+## Usage
+
+To run this example you need to execute:
+
+```bash
+$ terraform init
+$ terraform plan
+$ terraform apply
+```
+
+Note that this example may create resources which can cost money. Run `terraform destroy` when you don't need these
+resources.
+
+## Requirements
+
+| Name | Version |
+|------|---------|
+| terraform | > = 0.14.5 |
+| aws | > = 2.70 |
+
+## Providers
+
+| Name | Version |
+|------|---------|
+| aws | > = 2.65 |
+
+## Inputs
+
+- Region (default ap-southeast-1)
+
+## Outputs
+
+- EC2 Public IP Address
+
---
+++ main.tf
@@ -0,0 +1,55 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws",
+      version = "~> 2.70"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region = var.region
+}
+
+resource "aws_security_group" "web-server-security-group" {
+  name = "web-server-security-group"
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = [
+      "0.0.0.0/0"]
+  }
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = [
+      "0.0.0.0/0"]
+  }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = [
+      "0.0.0.0/0"]
+  }
+}
+
+
+resource "aws_instance" "web-server" {
+  ami = "ami-06fb5332e8e3e577a"
+  instance_type = "t2.micro"
+  key_name = "aws-demo"
+  vpc_security_group_ids = [
+    aws_security_group.web-server-security-group.id]
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 80 &
+              EOF
+  tags = {
+    Name = "web-server"
+  }
+}
